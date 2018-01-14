263c1b5e0d612abb2667ae0921a0e51e439c50fe
Antti Partanen <aehparta@iki.fi>
2018-01-15 01:03:19 +0200

    hmm

---
+++ base.php
@@ -0,0 +1,12 @@
+<?php
+
+ncurses_assume_default_colors(ncurses_assume_default_colors(fg, bg), bg)
+function get_entity_manager()
+{
+	return null;
+}
+
+function get_document_manager()
+{
+	return null;
+}
