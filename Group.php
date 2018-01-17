fc9bbbe8ab1ccbd75be9693ca952525ae873a9b9
Antti Partanen <aehparta@iki.fi>
2018-01-17 13:25:32 +0200

    just commit

--- Group.php
+++ Group.php
@@ -4,5 +4,34 @@ namespace Data;
 
 class Group
 {
+	/**
+	 * @var integer
+	 */
+	private $id;
+
+	/**
+	 * @var integer
+	 */
+	private $parent;
+
+	/**
+	 * @var string
+	 */
+	private $label;
+
+	/**
+	 * @var text
+	 */
+	private $description;
+
+	/**
+	 * @var \asdasd
+	 */
+	private $children;
+
+	/**
+	 * @var \asdasd
+	 */
+	private $tags;
 
 }
